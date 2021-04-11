4176e497e47407ab8e067a5e156d9a04fd06de31
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-04-11 16:53:40 +0900

    手順１３終了

--- Assets/Scripts/EventData.cs
+++ Assets/Scripts/EventData.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
-
+/// <summary>
+/// ƒCƒxƒ“ƒg‚ÌŽí—Þ
+/// </summary>
 [System.Serializable]
 public class EventData
 {
