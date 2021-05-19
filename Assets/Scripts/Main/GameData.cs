1019644f221c3300494f84e575868dc9d3766ccb
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-05-19 13:38:37 +0900

    手順２２の３８まで終了

--- Assets/Scripts/Main/GameData.cs
+++ Assets/Scripts/Main/GameData.cs
@@ -7,20 +7,25 @@ using System.Linq;
 public class GameData : MonoBehaviour
 {
     public static GameData instance;        // シングルトンデザインパターンのクラスにするための変数
-    public int randomEncountRate;           // エンカウントの発生率
+
+    public int randomEncountRate;　　　　　 // エンカウントの発生率
+
     public bool isEncouting;                // エンカウントしている状態かどうかの判定用。true の場合エンカウントしている状態
+
     public bool isDebug;                    // デバッグ用の変数。true ならば、エンカウントしている状態をリセットできる
+
     private Vector3 currentPlayerPos;       // エンカウント時のプレイヤーキャラの位置の情報を保持するための変数
-    private Vector2 currentLookDirection;    // エンカウント時のプレイヤーキャラの方向の情報を保持するための変数
+
+    private Vector2 currentLookDirection;　 // エンカウント時のプレイヤーキャラの方向の情報を保持するための変数
+
 
     [System.Serializable]
     public class ItemInventryData
     {
-        public ItemName itemName;    //アイテムの名前
+        public ItemName itemName;    // アイテムの名前
         public int count;            // 所持数
         public int number;           // 所持している通し番号
     }
-
 
     [Header("所持アイテムのリスト")]
     public List<ItemInventryData> itemInventryDatasList = new List<ItemInventryData>();
@@ -44,9 +49,9 @@ public class GameData : MonoBehaviour
     /// </summary>
     /// <param name="encountPlayerPos"></param>
     /// <param name="encountLookDirection"></param>
-
     public void SetEncountPlayerPosAndDirection(Vector3 encountPlayerPos, Vector2 encountLookDirection)
     {
+
         // エンカウント時のプレイヤーキャラの位置と方向の情報が引数で届くので、それらを変数に保持
         currentPlayerPos = encountPlayerPos;
         currentLookDirection = encountLookDirection;
@@ -58,13 +63,12 @@ public class GameData : MonoBehaviour
     /// エンカウント時のプレイヤーキャラの位置情報を戻す
     /// </summary>
     /// <returns></returns>
-
     public Vector3 GetCurrentPlayerPos()
     {
+
         // 保持している情報を戻す
         return currentPlayerPos;
     }
-
 
     /// <summary>
     /// エンカウント時のプレイヤーキャラの方向を戻す
@@ -72,6 +76,7 @@ public class GameData : MonoBehaviour
     /// <returns></returns>
     public Vector2 GetCurrentLookDirection()
     {
+
         // 保持している情報を戻す
         return currentLookDirection;
     }
@@ -80,15 +85,61 @@ public class GameData : MonoBehaviour
     /// ItemInvetryDatasList の最大数を取得
     /// </summary>
     /// <returns></returns>
-
     public int GetItemInventryListCount()
     {
         return itemInventryDatasList.Count;
     }
 
-
+    /// <summary>
+    /// ItemInvetryDatasList の中から指定した要素番号の ItemInventryData 情報を取得
+    /// </summary>
+    /// <param name="no"></param>
+    /// <returns></returns>
     public ItemInventryData GetItemInventryData(int no)
     {
         return itemInventryDatasList[no];
     }
+
+
+    ////*  ここからメソッドを２つ追加  *////
+
+
+    /// <summary>
+    /// 所持アイテムのセーブ
+    /// </summary>
+    
+    public void SaveItemInventryDatas()
+    {
+        
+        // 所持しているアイテムの数だけ処理を行う
+        for (int i = 0; i < itemInventryDatasList.Count; i++)
+        {
+            
+            // 所持しているアイテムの情報を１つの文字列としてセーブするための準備を行う
+            PlayerPrefs.SetString(itemInventryDatasList[i].itemName.ToString(), itemInventryDatasList[i].itemName.ToString() + "," + itemInventryDatasList[i].count.ToString() + "," + i.ToString());
+
+            Debug.Log(itemInventryDatasList[i].itemName.ToString());
+            Debug.Log(itemInventryDatasList[i].itemName.ToString() + "," + itemInventryDatasList[i].count.ToString() + "," + i.ToString());
+        }
+            // セーブ
+            PlayerPrefs.Save();
+
+        Debug.Log("ItemInventry セーブ完了");
+    }
+
+    // デバッグ用
+    private void Update()
+    {
+        
+        // デバッグ用　セーブ
+        if (Input.GetKeyDown(KeyCode.K) && isDebug)
+        {
+            
+            SaveItemInventryDatas();
+            
+        }
+    }
 }
+
+
+
--- Assets/Scripts/Main/ItemDataSO.cs
+++ Assets/Scripts/Main/ItemDataSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using OneLine;　　　　//　<=　①　宣言を追加してください
 
 [CreateAssetMenu(fileName = "ItemDataSO", menuName = "Create ItemDataSO")]
 public class ItemDataSO : ScriptableObject
@@ -19,7 +20,7 @@ public class ItemDataSO : ScriptableObject
 
         // TODO 必要な情報を追加する
     }
-
+    [OneLine] // <=　②　OneLine 属性か、OneLineWithHeader 属性のいずれかを追加してください
     public List<ItemData> itemDataList = new List<ItemData>();  // 複数のItemDataを１つの変数内でまとめて管理を行う
 
 }
